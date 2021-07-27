ddf22f4e7a54e558bcd8fd14209adcdb0474f1a6
Animesh <er.animesh6@hotmail.com>
2021-07-27 10:52:30 +0530

    code added.

--- recursion.py
+++ recursion.py
@@ -4,15 +4,15 @@ def find_sum(n):
     return n + find_sum(n - 1)
 
 
-def fib(n):
-    # 0,1,1,2,3,5,8 <-- fibonacci numbers
-    # --------------
-    # 0,1,2,3,4,5,6 <-- index
-    if n == 0 or n == 1:
+def fibo(n):
+    if n < 0:
+        print("Invalid input")
+    elif n == 0 or n == 1:
         return n
-    return fib(n - 1) + fib(n - 2)
+    else:
+        return fibo(n - 1) + fibo(n - 2)
 
 
 if __name__ == '__main__':
     print(find_sum(5))
-    print(fib(10))
+    print(fibo(10))
