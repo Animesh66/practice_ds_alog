0ac3c81dc7b21c0898e1509b9724e008cd2d0edc
Animesh <er.animesh6@hotmail.com>
2021-08-09 17:35:00 +0530

    code added.

---
+++ keypad_automation.py
@@ -0,0 +1,14 @@
+user_input = input("Enter a string: ")
+mobile_key = {'ABC': 2, 'DEF': 3, 'GHI': 4, 'JKL': 5, 'MNO': 6, 'PQRS': 7, 'TUV': 8, 'WXYZ': 9}
+user_output = []
+
+
+def test_function(letters):
+    for letter in letters:
+        for l, v in mobile_key.items():
+            if letter in mobile_key[l]:
+                return user_output.append(v)
+
+
+
+print(test_function(user_input))
