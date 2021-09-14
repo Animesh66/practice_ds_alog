7312ae102d384a42bab84794e8e16392a5d4bc90
Animesh <er.animesh6@hotmail.com>
2021-09-14 11:18:09 +0530

    code added.

--- regular_expression.py
+++ regular_expression.py
@@ -5,7 +5,7 @@ Animesh is 30 and Abhishek is 34
 Supriti is 31 and Tanmoy is 33
 """
 
-names = re.findall(r'[A-Z][a-z]*]', name_age)  # returns a list with uppercase letters from name_age string
+names = re.findall(r'[A-Z][a-z]*', name_age)  # returns a list with uppercase letters from name_age string
 ages = re.findall(r'\d{1,3}', name_age)  # returns a list with 2 digit number from name_age string
 
 age_dict = {}  # initialize an empty dictionary
