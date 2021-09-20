0c03b34e255724ee97a1242050bd637c63c0aeec
Animesh <er.animesh6@hotmail.com>
2021-09-20 20:40:55 +0530

    code added.

--- practice_new.py
+++ practice_new.py
@@ -1,15 +1,18 @@
 def return_frequency():
-    word = input("Enter a string: ")
-    new_string = word.lower()
-    converted_string = new_string.replace(" ", "")
-    char_frequency = {}
-    for char in converted_string:
-        if char in char_frequency:
-            char_frequency[char] += 1
-        else:
-            char_frequency[char] = 1
-    sorted_char_frequency = sorted(char_frequency.items(), key=lambda kv: kv[1])
-    return sorted_char_frequency[-1]
+    try:
+        word = input("Enter a string: ")
+        new_string = word.lower()
+        converted_string = new_string.replace(" ", "")
+        char_frequency = {}
+        for char in converted_string:
+            if char in char_frequency:
+                char_frequency[char] += 1
+            else:
+                char_frequency[char] = 1
+        sorted_char_frequency = sorted(char_frequency.items(), key=lambda kv: kv[1])
+        return sorted_char_frequency[-1]
+    except IndexError:
+        print("Invalid string")
 
 
 if __name__ == "__main__":
